ebf8d03e76b859541abfb8166baa5abecb2f7808
Simon Kers <simon.kers@hiq.se>
2017-09-26 15:15:12 +0200

    Using analytics with react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
+import { Router, Route, hashHistory } from 'react-router'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactGA.initialize('UA-819513-8')
+
+function logPageView () {
+  ReactGA.set({ page: window.location.pathname + window.location.search })
+  ReactGA.pageview(window.location.pathname + window.location.search)
+}
+
+ReactDOM.render(
+  <Router history={hashHistory} onUpdate={logPageView}>
+    <Route path='/' component={App} />
+  </Router>,
+  document.getElementById('root')
+)
 registerServiceWorker()
